f6433d978cfbbb3013985c9e5059c8b6843f6072
virginia-njeri <virginiaanjeri@gmail.com>
2022-02-17 11:51:31 +0300

    student record

---
+++ src/record.kt
@@ -0,0 +1,22 @@
+import kotlin.test.assertFalse
+
+fun main() {
+    var name="mary wangare"
+    println(name)
+    var age="24"
+    println(age)
+
+    var phonenumber="0757552577"
+    println(phonenumber)
+
+    var weight:String="58"
+    println(weight)
+
+    var kenyan:Boolean=true
+    println(kenyan)
+
+
+
+
+
+}
